05fbf43e5f8aa74620f72c7d35feef9b34c301f4
Guru Raghupathy <guru@doit-intl.com>
2024-03-13 06:09:35 +0000

    Added

--- src/main/java/com/alacritysys/developer/App.java
+++ src/main/java/com/alacritysys/developer/App.java
@@ -1,9 +1,13 @@
 package com.alacritysys.developer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
 	/*
@@ -11,6 +15,6 @@ public class App
 	 */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
