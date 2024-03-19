14359543c02320d8f841f568c3b8815fc66ccd74
Guru Raghupathy <guru@doit-intl.com>
2024-03-19 12:39:09 +0000

    Annotation Check

--- src/test/java/com/alacritysys/developer/cucumber/CucumberRunnerTest.java
+++ src/test/java/com/alacritysys/developer/cucumber/CucumberRunnerTest.java
@@ -1,5 +1,7 @@
 package com.alacritysys.developer.cucumber;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
@@ -10,4 +12,8 @@ import org.junit.platform.suite.api.Suite;
 @SelectPackages("com.alacritysys.developer.cucumber")
 public class CucumberRunnerTest {
 
+	@Test
+	public void testCucumberWorks() {
+		Assertions.assertThat(this.getClass().getAnnotations()).isNotEmpty();
+	}
 }
